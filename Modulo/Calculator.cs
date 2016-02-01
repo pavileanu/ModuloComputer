2f001c89e36c341676998294d356705d4bf0f807
Mircea Pavileanu <mircea.pavileanu@internal.eminus-software.com>
2016-02-01 11:25:50 +0200

    Computer Begining

---
+++ Modulo/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Modulo
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void getValueClick(object sender, System.EventArgs e)
+        {
+            textBox1.Text = Modulo.Operation.Modulo.modOf(Convert.ToInt64(textBox1.Text)).ToString();
+        }
+
+        private void setModuloBlur(object sender, EventArgs e)
+        {
+            Modulo.Operation.Modulo.ModuloSetNumber = Convert.ToInt64(textBox2.Text);
+        }
+        
+    }
+
+}
---
+++ Modulo/Operation/Modulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulo.Operation
+{
+    public class Modulo
+    {
+        public static long ModuloSetNumber{get; set;}
+
+        public static long modOf(long a)
+        {
+            return a % ModuloSetNumber;
+        }
+    }
+}
